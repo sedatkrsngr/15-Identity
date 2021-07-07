61acfaddd9308221222ac87d7917a1e21849e66b
sedatkrsngr <limitless_fearless@hotmail.com>
2021-07-07 15:06:26 +0300

    Başarısız Giriş Sayısına Göre Hesap Kitleme

--- Identity/Identity.web/Controllers/HomeController.cs
+++ Identity/Identity.web/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Identity.web.Controllers
         private readonly UserManager<AppUser> _userManager;//kullanıcı bilgilerini getirir
         private readonly SignInManager<AppUser> _signInManager;//login işlemlerini getirir
 
-        public HomeController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        public HomeController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -28,7 +28,7 @@ namespace Identity.web.Controllers
         }
         public IActionResult SignUp()
         {
-            
+
             return View();
         }
 
@@ -43,7 +43,7 @@ namespace Identity.web.Controllers
                 appUser.PhoneNumber = userViewModel.PhoneNumber;
                 appUser.Email = userViewModel.Email;
 
-               IdentityResult result = await _userManager.CreateAsync(appUser,userViewModel.Password);//şifreyi böyle giriyoruz çünkü şifreleme olarak kaydedilmesi gerekiyor
+                IdentityResult result = await _userManager.CreateAsync(appUser, userViewModel.Password);//şifreyi böyle giriyoruz çünkü şifreleme olarak kaydedilmesi gerekiyor
 
                 if (result.Succeeded)
                 {
@@ -75,27 +75,52 @@ namespace Identity.web.Controllers
             {
                 AppUser appUser = await _userManager.FindByEmailAsync(loginViewModel.Email);
 
-                if (appUser!=null)
+                if (appUser != null)
                 {
+
+                    if (await _userManager.IsLockedOutAsync(appUser))
+                    {
+
+                        ModelState.AddModelError("", "Hesabınız bir süreliğine kitlenmiştir. Lütfen daha sonra tekrar deneyiniz");
+                    }
+
                     //cookileri sileriz başlangıçta
                     await _signInManager.SignOutAsync();
-                   
-                    Microsoft.AspNetCore.Identity.SignInResult result =  await _signInManager.PasswordSignInAsync(appUser, loginViewModel.Password, loginViewModel.RememberMe, true);
+
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(appUser, loginViewModel.Password, loginViewModel.RememberMe, true);
                     //ilk boolena olan beni hatırlaya bağlı olmalı eğer beni hatırla olmaz ise kapattığımızda bilgiler uçar,
                     //ikinci boolean olan ise hatalı giriş yapılıncı hesabı belli bir süre kitler.
 
                     if (result.Succeeded)
                     {
-                        if (TempData["ReturnUrl"]!=null)//işlem başarılı olduğunda ilgili sayfaya geri gitsin
+                        await _userManager.ResetAccessFailedCountAsync(appUser);// başarısız giriş sayısını sıfırlarız
+                        if (TempData["ReturnUrl"] != null)//işlem başarılı olduğunda ilgili sayfaya geri gitsin
                         {
                             return Redirect(TempData["ReturnUrl"].ToString());
                         }
-                        return RedirectToAction("Index","Member");
+                        return RedirectToAction("Index", "Member");
+                    }
+                    else
+                    {
+                        await _userManager.AccessFailedAsync(appUser)//başarısız girişi 1 arttır
+                        int failCount = await _userManager.GetAccessFailedCountAsync(appUser);//Başarısız giriş sayısı
+                        ModelState.AddModelError("", $"{failCount} kez başarısız giriş");
+                        if (failCount == 3)
+                        {
+                            await _userManager.SetLockoutEndDateAsync(appUser, new DateTimeOffset(DateTime.Now.AddMinutes(20)));//20 dk lığına kitledik
+
+                            ModelState.AddModelError("", $"Hesabınız {failCount} başarısız girişten dolayı 20 dk kitlenmiştir. Lütfen daha sonra tekrar deneyiniz");
+                            return View(loginViewModel);
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Yanlış email adresi veya şifresi");
+                        }
                     }
                 }
                 else
                 {
-                        ModelState.AddModelError("","Geçersiz email adresi veya şifresi");
+                    ModelState.AddModelError("", "Bu Email adresine kayıtlı kullanıcı bulunamamıştır.");
                     //Hata summaryde görünür ama nameof(loginViewModel.Email) yazarsak "" yerine mail altında görünür
                 }
 
